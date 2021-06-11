f3bdbe4b8cc7a6b5d51b1cb558fb6152528a794e
Bianca Severino <biancasev@gmail.com>
2021-06-11 17:57:07 -0400

    gHappy

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,48 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class GHappyTest {
+    @Test
+    public void onePair() {
+        GHappy g = new GHappy();
+        Boolean result = g.gHappy("xxggxx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void singleG() {
+        GHappy g = new GHappy();
+        Boolean result = g.gHappy("xxgxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void onePairAndSingleG() {
+        GHappy g = new GHappy();
+        Boolean result = g.gHappy("xxggyygxx");
+        Assertions.assertFalse(result);
+    }
+
+    @Test
+    public void triple() {
+        GHappy g = new GHappy();
+        Boolean result = g.gHappy("xxgggxx");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void shortString() {
+        GHappy g = new GHappy();
+        Boolean result = g.gHappy("gg");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void singleChar() {
+        GHappy g = new GHappy();
+        Boolean result = g.gHappy("g");
+        Assertions.assertFalse(result);
+    }
 }
