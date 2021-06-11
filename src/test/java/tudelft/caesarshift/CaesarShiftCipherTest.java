db5a26ead5928a70f39510d1394f4e58060a81ff
Bianca Severino <biancasev@gmail.com>
2021-06-11 18:15:30 -0400

    CaesarShiftCipher

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,28 @@
 package tudelft.caesarshift;
 
-public class CaesarShiftCipherTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+public class CaesarShiftCipherTest {
+    @ParameterizedTest(name = "message")
+    @CsvSource({ "UPPERCASE", "12345", "$&*%()@" })
+    public void invalidCharacters(String message) {
+        String result = new CaesarShiftCipher().CaesarShiftCipher(message, 1);
+        Assertions.assertEquals("invalid", result);
+    }
+
+    @Test
+    public void noWrap() {
+        String result = new CaesarShiftCipher().CaesarShiftCipher("abcdef", 5);
+        Assertions.assertEquals("fghijk", result);
+    }
+
+    @ParameterizedTest(name = "message={0}, shift={1}, expectedResult={2}")
+    @CsvSource({ "abc, -1, zab", "xyz, 1, yza" })
+    public void wrapAround(String message, int shift, String expectedResult) {
+        String result = new CaesarShiftCipher().CaesarShiftCipher(message, shift);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
