6223fa7807538f255f1cbc82ecf28cb798585bfc
Bianca Severino <biancasev@gmail.com>
2021-06-16 19:09:14 -0400

    CountLetters

--- src/test/java/tudelft/countletters/CountLettersTest.java
+++ src/test/java/tudelft/countletters/CountLettersTest.java
@@ -2,19 +2,30 @@ package tudelft.countletters;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class CountLettersTest {
 
-    @Test
-    public void multipleMatchingWords() {
-        int words = new CountLetters().count("cats|dogs");
+    @ParameterizedTest(name = "string")
+    @CsvSource({ "cats|dogs", "car|door" })
+    public void multipleMatchingWords(String string) {
+        int words = new CountLetters().count(string);
         Assertions.assertEquals(2, words);
     }
 
-    @Test
-    public void lastWordDoesNotMatch() {
-        int words = new CountLetters().count("cats|dog");
+    @ParameterizedTest(name = "string")
+    @CsvSource({ "cats|dog", "car|dog" })
+    public void lastWordDoesNotMatch(String string) {
+        int words = new CountLetters().count(string);
         Assertions.assertEquals(1, words);
+    }
+
+    @ParameterizedTest(name = "string")
+    @CsvSource({ "cat|dog", "cat" })
+    public void noMatchingWords(String string) {
+        int words = new CountLetters().count(string);
+        Assertions.assertEquals(0, words);
     }
 
 }
